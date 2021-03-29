fd53dcd6e223ffeed6c4eb16c1835b60a8b791a0
JarbasAI <33701864+jarbasal@users.noreply.github.com>
2021-03-29 23:43:10 +0100

    feat/locale_switching + packaging/LN+LF_support
    - integration of timezone support with LN
    - lingua_franca and lingua_nostra need to be able to coexist
    	- skills might be importing one of those libs directly
    - HolmesV will call property setters (lang/timezone) for both libs if possible
    - all other wrapped methods (`mycroft.util.parse` + `mycroft.util.format` + `mycroft.util.time`) will give preference to LN if it is available, use LF otherwise
    	- this should be irrelevant for end users since LN is a drop in replacement

--- mycroft/configuration/locale.py
+++ mycroft/configuration/locale.py
@@ -18,10 +18,123 @@
 The mycroft.util.lang module provides the main interface for setting up the
 lingua-franca (https://github.com/mycroftai/lingua-franca) selected language
 """
+from dateutil.tz import gettz, tzlocal
+from mycroft.configuration.config import Configuration
+# we want to support both LF and LN
+# when using the mycroft wrappers this is not an issue, but skills might use
+# either, so mycroft-lib needs to account for this and set the defaults for
+# both libs.
 
-from lingua_franca import set_default_lang as _set_default_lf_lang
+# lingua_franca/lingua_nostra are optional and might not be installed
+# exceptions should only be raised in the parse and format utils
 
 
+try:
+    import lingua_franca as LF
+except ImportError:
+    LF = None
+
+try:
+    import lingua_nostra as LN
+except ImportError:
+    LN = None
+
+_lang = "en-us"
+_tz = tzlocal()
+
+
+def get_primary_lang_code():
+    if LN:
+        return LN.get_primary_lang_code()
+    if LF:
+        return LF.get_primary_lang_code()
+    return _lang.split("-")[0]
+
+
+def get_default_lang():
+    if LN:
+        return LN.get_default_lang()
+    if LF:
+        return LF.get_default_lang()
+    return _lang
+
+
+def set_default_lang(lang):
+    global _lang
+    _lang = lang
+    if LN:
+        LN.set_default_lang(lang)
+    if LF:
+        LF.set_default_lang(lang)
+
+
+def get_config_tz():
+    config = Configuration.get()
+    code = config["location"]["timezone"]["code"]
+    return gettz(code)
+
+
+def get_default_tz():
+    tz = None
+    # go with LF/LN default timezone, in case skills changed it temporarily
+    # for some arcane reason...
+    if LN:
+        tz = tz or LN.time.default_timezone()
+    if LF:
+        try:
+            tz = tz or LF.time.default_timezone()
+        except Exception:
+            # old versions of LF
+            # AttributeError: module 'lingua_franca' has no attribute 'time'
+            pass
+
+    # use the timezone from .conf
+    tz = tz or get_config_tz()
+
+    # Just go with global default
+    return tz or _tz
+
+
+def set_default_tz(tz=None):
+    """ configure both LF and LN """
+    global _tz
+    tz = tz or get_config_tz() or tzlocal()
+    _tz = tz
+    if LN:
+        LN.time.set_default_tz(tz)
+    if LF:
+        # tz added in recently, depends on version
+        try:
+            LF.time.set_default_tz(tz)
+        except:
+            pass
+
+
+def load_languages(langs):
+    if LN:
+        LN.load_languages(langs)
+    if LF:
+        LF.load_languages(langs)
+
+
+def load_language(lang):
+    if LN:
+        LN.load_language(lang)
+    if LF:
+        LF.load_language(lang)
+
+
+def setup_locale(lang=None, tz=None):
+    lang_code = lang or Configuration.get().get("lang", "en-us")
+    # Load language resources, currently en-us must also be loaded at all times
+    load_languages([lang_code, "en-us"])
+    # Set the active lang to match the configured one
+    set_default_lang(lang_code)
+    # Set the default timezone to match the configured one
+    set_default_tz(tz)
+
+
+# mycroft-core backwards compat LF only interface
 def set_default_lf_lang(lang_code="en-us"):
     """Set the default language of Lingua Franca for parsing and formatting.
 
@@ -32,4 +145,4 @@ def set_default_lf_lang(lang_code="en-us"):
     Args:
         lang (str): BCP-47 language code, e.g. "en-us" or "es-mx"
     """
-    return _set_default_lf_lang(lang_code=lang_code)
+    return set_default_lang(lang_code)
