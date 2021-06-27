51e48cfc1c17084aed77e6407a5280394e45b827
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-06-27 02:43:52 -1000

    feat/support chatterbox-bus-client

--- mycroft/messagebus/client/client.py
+++ mycroft/messagebus/client/client.py
@@ -12,27 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+from mycroft.messagebus.load_config import get_bus_config
+from mycroft.util.log import LOG
+from mycroft.util.process_utils import create_echo_function
 from mycroft_bus_client import MessageBusClient as _MessageBusClient
 from mycroft_bus_client.client import MessageWaiter
 
-from mycroft.messagebus.load_config import load_message_bus_config
-from mycroft.util.process_utils import create_echo_function
-
 
 class MessageBusClient(_MessageBusClient):
-    # minimize reading of the .conf
-    _config_cache = None
+    def __new__(cls, *args, **kwargs):
+        # get default arguments from config
+        bus_config = get_bus_config(kwargs)
+        kwargs["host"] = kwargs.get("host") or bus_config.get("host",
+                                                              "0.0.0.0")
+        kwargs["port"] = kwargs.get("port") or bus_config.get("port", 8181)
+        kwargs["route"] = kwargs.get("route") or bus_config.get("route",
+                                                                "/core")
+        kwargs["ssl"] = kwargs.get("ssl") or bus_config.get("ssl", False)
 
-    def __init__(self, host=None, port=None, route=None, ssl=None, cache=False):
-        config_overrides = dict(host=host, port=port, route=route, ssl=ssl)
-        if cache and self._config_cache:
-            config = self._config_cache
-        else:
-            config = load_message_bus_config(**config_overrides)
-            if cache:
-                MessageBusClient._config_cache = config
-        super().__init__(config.host, config.port, config.route, config.ssl)
+        if bus_config.get("use_chatterbox"):
+            LOG.info("Using chatterbox_bus_client")
+            # chatterbox bus client is a drop in replacement, it adds functionality
+            # to encrypt payloads, this is done transparently from the .conf
+            from chatterbox_bus_client import \
+                MessageBusClient as ChatterboxMessageBusClient
+            return ChatterboxMessageBusClient(*args, **kwargs)
+        return _MessageBusClient(*args, **kwargs)
 
 
 def echo():
